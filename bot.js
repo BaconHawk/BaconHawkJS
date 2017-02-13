d37c5ef3d30e0e72e818e6bca8e5822fc3d6c48a
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-02-13 18:11:21 -0500

    Updated and Added a working command

--- bot.js
+++ bot.js
@@ -1,14 +1,16 @@
 //BaconHawkJS Bot Frame Work by Bacon_Space & NiteHawk Please refer to https://github.com/BaconHawk/BaconHawkJS for help!
 
+var chalk = require("chalk");
+var logError = chalk.red('ERR!') + chalk.white
 var log = console.log
 var version = "v0.0.1"
 
 try{
 	var Discord = require("discord.js");
 } catch(e) {
-	log(e.stack);
-	log(process.version);
-	logError('Please run npm install and make sure it passes with NO errors!');
+	log(logError(e.stack));
+	log(logError(process.version));
+	log('Please run npm install and make sure it passes with NO errors!');
 	process.exit();
 }
 
@@ -18,7 +20,7 @@ log('Starting BaconHawkJS\nBaconHawkJS version: ' + version + '\nDiscordJS versi
 try{
 	var AuthData = require("./auth.json");
 } catch(e) {
-	log('Please create an auth.json file in the main directory based off of the auth.json.example file!\n' + e.stack);
+	log(logError('Please create an auth.json file in the main directory based off of the auth.json.example file!\n' + e.stack));
 	process.exit();
 }
 
@@ -26,14 +28,45 @@ try{
 try{
 	var Config = require("./config.json");
 } catch(e) {
-	log('Please create an config.json file in the main directory based off of the config.json.example file!\n' + e.stack);
+	log(logError('Please create an config.json file in the main directory based off of the config.json.example file!\n' + e.stack));
 	process.exit();
+}
+
+var commands = {
+	"ping": {
+		run: function(bot,suffix,msg) {
+			msg.reply('Pong!');
+		}
+	}
 }
 
 var bot = new Discord.Client();
 
+function checkForCommand(msg) {
+	if(msg.author.id != bot.user.id && msg.content.startsWith(Config.prefix)){
+		
+		var cmdTxt = msg.content.split(" ")[0].substring(Config.prefix.length);
+		var suffix = msg.content.substring(cmdTxt.length+Config.prefix.length+1);
+		
+		log('Running ' + cmdTxt + `, from ${msg.author.id}, as a command!`);
+		
+		var cmd = commands[cmdTxt];
+		
+		if(cmd){
+			try{
+				cmd.run(bot,suffix,msg);
+			} catch(e) {
+				var msgTxt = 'Command ' + cmdTxt + ' was unable to run!';
+				msg.channel.sendMessage(msgTxt);
+			}
+		}
+	}
+}
+
+bot.on("message", msg => checkForCommand(msg, false));
+
 if(AuthData.bot_token){
 	bot.login(AuthData.bot_token);
 } else{
-	log('Please Provide a bot token in auth.json');
+	log(logError('Please Provide a bot token in auth.json'));
 }
