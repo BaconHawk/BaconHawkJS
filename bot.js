1e9957e9ce84f63bb21b6cead830d7f2ea3a3fa0
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-02-14 23:41:07 -0500

    Made things look nicer!

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
 //BaconHawkJS Bot Frame Work by Bacon_Space & NiteHawk Please refer to https://github.com/BaconHawk/BaconHawkJS for help!
 
-const chalk = require("chalk");
+const chalk = require('chalk');
 const logError = chalk.red('ERR!') + chalk.white
 const log = console.log
-const Package = require("./package.json");
-const Discord = require("discord.js");
-let AuthData; //Will be removed in future update
+const Package = require('./package.json');
+const Discord = require('discord.js');
+let AuthData; //will be removed in coming versions
 let Config;
 
-log('Starting BaconHawkJS\nBaconHawkJS version: ' + Package.version + '\nDiscordJS version: v' + Discord.version);
+log(`Starting BaconHawkJS\nBaconHawkJS version: ${Package.version}\nDiscordJS version: v${Discord.version}`);
 
 //Get Auth Data from Auth.json
 try{
-	AuthData = require("./auth.json");
+	AuthData = require('./auth.json');
 } catch(e) {
 	log(logError('Please create an auth.json file in the main directory based off of the auth.json.example file!\n' + e.stack));
 	process.exit();
@@ -20,7 +20,7 @@ try{
 
 //Get Config Data from config.json
 try{
-	Config = require("./config.json");
+	Config = require('./config.json');
 } catch(e) {
 	log(logError('Please create an config.json file in the main directory based off of the config.json.example file!\n' + e.stack));
 	process.exit();
@@ -45,10 +45,10 @@ const bot = new Discord.Client();
 function checkForCommand(msg) {
 	if(msg.author.id != bot.user.id && msg.content.startsWith(Config.prefix)){
 		
-		let cmdTxt = msg.content.split(" ")[0].substring(Config.prefix.length);
+		let cmdTxt = msg.content.split(' ')[0].substring(Config.prefix.length);
 		let suffix = msg.content.substring(cmdTxt.length+Config.prefix.length+1);
 		
-		log('Running ' + cmdTxt + `, from ${msg.author.id}, as a command!`);
+		log(`Running ${cmdTxt}, from ${msg.author.id}, as a command!`);
 		
 		let cmd = commands[cmdTxt];
 		
@@ -56,15 +56,15 @@ function checkForCommand(msg) {
 			try{
 				cmd.run(bot,suffix,msg);
 			} catch(e) {
-				var msgTxt = 'Command ' + cmdTxt + ' was unable to run!';
+				var msgTxt = `Command ${cmdTxt} was unable to run!`;
 				msg.channel.sendMessage(msgTxt);
 			}
 		}
 	}
 }
 
-bot.on("message", msg => checkForCommand(msg));
-bot.on("ready", () => {log(`Your bot is ready and running on \n ${bot.guilds.size} servers \n with ${bot.channels.size} channels \n for ${bot.users.filter(user => !user.bot).size} users!`)});
+bot.on('message', msg => checkForCommand(msg));
+bot.on('ready', () => {log('Your bot is ready and running on ' + chalk.blue(`${bot.guilds.size}`) + ' servers with ' + chalk.blue(`${bot.channels.size}`) + ' channels for ' + chalk.blue(`${bot.users.filter(user => !user.bot).size}`) + ' users!')});
 
 if(AuthData.bot_token){
 	bot.login(AuthData.bot_token);
