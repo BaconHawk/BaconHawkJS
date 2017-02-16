59ece980d9396a856b396309487f791d4376915e
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-02-15 19:37:44 -0500

    No longer needed

--- modules/info/about.js
+++
@@ -1,7 +0,0 @@
-exports.command = "about";
-
-exports.about = {
-	run: function(bot,suffix,msg) {
-		msg.reply('Feel Free To Check Out My New Framework Right Here https://github.com/BaconHawk/BaconHawkJS/');
-	}
-}
