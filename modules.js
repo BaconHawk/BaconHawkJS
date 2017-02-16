f8b3f7495cc3fe48bbcc294d5691785456244b4f
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-02-15 19:35:47 -0500

    Added support for the reload command

--- modules.js
+++ modules.js
@@ -7,16 +7,40 @@ exports.init = function(){
     load_modules();
 };
 
+exports.reload = function() {
+	reload_modules();
+};
+
 function getDir(srcpath) {
     return fs.readdirSync(srcpath).filter(function(file) {
         return fs.statSync(path.join(srcpath, file)).isDirectory();
     });
 }
 
-
-
 const mainDir = './modules/';
 const mainDirs = getDir(mainDir);
+
+function reload_modules() {
+	for (let i = 0; i<mainDirs.length; i++) {
+		function getFile(srcpath) {
+			return fs.readdirSync(srcpath).filter(function(file) {
+				return fs.statSync(path.join(srcpath, file)).isFile();
+			});
+		}
+		
+		const moduleFile = getFile(mainDir + mainDirs[i]);
+		
+		for (let j = 0; j<moduleFile.length; j++) {
+			
+			try {
+				delete require.cache[require.resolve(mainDir + mainDirs[i] + '/' + moduleFile[j])];
+			} catch(err) {
+				
+			}
+		}
+	}
+	load_modules();
+}
 
 
 function load_modules() {
@@ -45,5 +69,7 @@ function load_modules() {
 			}
 		}
 	}
+	exports.commandCount = commandCount;
 	console.log(chalk.blue('Success!') + ` ${commandCount} modules have been loaded!`)
 }
+
