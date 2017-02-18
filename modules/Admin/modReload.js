2a25d07be163a21014929c8c33b787670e93c4bf
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-02-17 20:18:22 -0500

    Added check for owner

--- modules/Admin/modReload.js
+++ modules/Admin/modReload.js
@@ -7,15 +7,18 @@ const Config = require('../../config.json');
 exports.modReload = {
 	run: function(bot,suffix,msg) {	
 		if(msg.author.id === Config.ownerID){
-		mod.reload()
-		const embed = new Discord.RichEmbed()
-			.setTitle('Module Reload')
-			.setDescription(`Modules Reloaded! Now Running ${mod.commandCount} modules!`)
-			.setFooter('BaconHawkJS')
-			.setTimestamp()
-			.setColor(0xbb0000);
+			mod.reload()
+			const embed = new Discord.RichEmbed()
+				.setTitle('Module Reload')
+				.setDescription(`Modules Reloaded! Now Running ${mod.commandCount} modules!`)
+				.setFooter('BaconHawkJS')
+				.setTimestamp()
+				.setColor(0xbb0000);
 
-		msg.channel.sendEmbed(embed)
-	}
+			msg.channel.sendEmbed(embed)
+		} else {
+			const embed = new Discord.RichEmbed()
+			msg.channel.sendEmbed(embed.setTimestamp().setTitle('Module Reload').addField('Error:', 'No Permission!').setColor(0xff0000).setFooter('BaconHawkJS'));
+		}
 	}
 }
