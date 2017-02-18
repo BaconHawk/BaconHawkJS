bb2de135fd03e17eb865286dd299be20ac9ecb3f
NITEHAWKDevs <nitehawk07gaming@gmail.com>
2017-02-17 20:10:07 -0500

    Added owner only use

--- modules/Admin/modReload.js
+++ modules/Admin/modReload.js
@@ -2,9 +2,11 @@ exports.command = 'modReload';
 
 const Discord = require('discord.js');
 const mod = require('../../modules.js');
+const Config = require('../../config.json');
 
 exports.modReload = {
 	run: function(bot,suffix,msg) {	
+		if(msg.author.id === Config.ownerID){
 		mod.reload()
 		const embed = new Discord.RichEmbed()
 			.setTitle('Module Reload')
@@ -14,5 +16,6 @@ exports.modReload = {
 			.setColor(0xbb0000);
 
 		msg.channel.sendEmbed(embed)
+	}
 	}
 }
